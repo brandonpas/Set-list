e448652dc81bf24468bcc3ba45190cd54f9c818b
Brandon <brandonpas22@gmail.com>
2017-07-21 22:30:04 -0400

    Added Kotterknife

--- app/src/main/java/com/gmail/pasquarelli/brandon/setlist/MainActivity.kt
+++ app/src/main/java/com/gmail/pasquarelli/brandon/setlist/MainActivity.kt
@@ -3,25 +3,26 @@ package com.gmail.pasquarelli.brandon.setlist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.view.MenuItem
 import android.widget.TextView
+import butterknife.bindView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
-    private var textMessagBox: TextView? = null
+    val textMessagBox: TextView by bindView(R.id.message)
+    val navigation: BottomNavigationView by bindView(R.id.navigation)
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessagBox!!.setText(R.string.title_home)
+                textMessagBox?.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                textMessagBox!!.setText(R.string.title_dashboard)
+                textMessagBox?.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                textMessagBox!!.setText(R.string.title_notifications)
+                textMessagBox?.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -31,9 +32,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textMessagBox = findViewById(R.id.message) as TextView
-        val navigation = findViewById(R.id.navigation) as BottomNavigationView
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
