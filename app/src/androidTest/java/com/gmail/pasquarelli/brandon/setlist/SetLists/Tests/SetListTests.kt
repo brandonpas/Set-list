5ff822856cc5ad83d8b044f736ce13cd59b94cda
Brandon <brandonpas22@gmail.com>
2017-08-31 16:26:16 -0400

    New manifest for instrumentation tests

--- app/src/androidTest/java/com/gmail/pasquarelli/brandon/setlist/SetLists/Tests/SetListTests.kt
+++ app/src/androidTest/java/com/gmail/pasquarelli/brandon/setlist/SetLists/Tests/SetListTests.kt
@@ -38,13 +38,13 @@ class SetListTests {
     @Throws(Exception::class)
     fun setUp() {
         val activity = activityRule.getActivity()
-        activityRule.runOnUiThread(Runnable {
+        activityRule.runOnUiThread({
             val mKG = activity.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
             val mLock = mKG.newKeyguardLock(KEYGUARD_SERVICE)
             mLock.disableKeyguard()
 
             //turn the screen on
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
+            activity.window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
         })
     }
 
